0d9602705eb26afd7177afb17669a865e5ce809f
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-07-25 10:45:15 -0400

    Database set up, first route in place

--- server/models.py
+++ server/models.py
@@ -6,3 +6,17 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+
+class Earthquake(db.Model):
+    __tablename__ = "earthquakes"  # Names the table
+
+    # Sets the columns
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+# Readable Output
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
